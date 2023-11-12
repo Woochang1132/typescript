153560126549ff7ca09b2c32330b1f28c6a80308
Woochang1132 <show4031132@gmail.com>
2023-11-12 23:52:15 +0900

    1-1 타입스크립트에서의 타입

--- 1-1-basic.ts
+++ 1-1-basic.ts
@@ -1,5 +1,30 @@
 {
     //JavaScript
-    //let => es6, 도입
-    //const
+    // let => es6, 도입
+    // const
+    // Primitive: number, string, boolean, bigint, symbol, null, undefined
+    // Object : function, array ...
+
+     
+    //number
+    const num:number=3;
+    
+    // string
+    const str:string ='hello';
+
+    //boolean
+    const boal: boolean = false;
+
+    // undefined
+    let age: number | undefined
+    age = undefined;
+    age =1;
+    function find(): number | undefined {
+        return undefined
+    }
+    
+    // null
+    let person : string | null;
+    person = null;
+
 }
