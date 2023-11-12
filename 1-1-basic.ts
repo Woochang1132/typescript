8c01b4890466488d96215652477f0f67c3d1c12c
Woochang1132 <show4031132@gmail.com>
2023-11-13 00:08:07 +0900

    1-1 타입스크립트 타입(추가)

--- 1-1-basic.ts
+++ 1-1-basic.ts
@@ -27,4 +27,33 @@
     let person : string | null;
     person = null;
 
+    // unknown
+    let notSure : unknown = 0;
+    notSure = 'he';
+
+    // any
+    let anyting : any = 0;
+    anyting = "hello"
+
+    // void
+    function print(): void {
+        console.log('hello');
+        return;
+    }
+
+    // never
+    function throwError(message: string): never{
+        // message -> server(log)
+        // 에러를 던지거나
+        // 어떠한 것도 리턴하지 않는 경우에 never
+        throw new Error(message);
+    }
+
+    // Object 
+    let obj : object = [1,4];
+    function acceptSomeObject(obj : object){
+
+    }
+    acceptSomeObject({name : 'woochang'});
+    acceptSomeObject({animal : 'dog'})
 }
