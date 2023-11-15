31d008134dc7e73c339b9d1168ce00374a19c8b9
Woochang1132 <show4031132@gmail.com>
2023-11-15 17:47:15 +0900

    typeScript => 캡슐화

--- 3-OOP/3-3-encapsulation.ts
+++ 3-OOP/3-3-encapsulation.ts
@@ -46,4 +46,29 @@
         //maker.coffeeBeans = 3; // 내부 상태를 외부에서 바꾸는 경우가 발생하게 됨
         maker.fillCoffeeBeans(32);
         console.log("maker >>" , maker)
+
+        class User{
+            get fullName(): string {
+                return `${this.firstName} ${this.lastName}`
+            }
+            private interalAge = 4;
+            get age() : number{
+                return this.interalAge;
+            }
+            set age(num : number){
+                if(num < 0){
+                    console.log("num is less than 0")
+                }
+                this.interalAge = num;
+            }
+            // 이렇게 설정하면 this.firstName = firstName을 함축하고 있다.
+            constructor(private firstName : string, public lastName : string){
+            }
+        }
+        const user = new User('Steve', 'Jobs');
+        user.age = 6
+        console.log("user get >>", user.age)
+        console.log("user >>", user);
+        //  get 함수에 접근할 때 => 멤버 변수에 접근하는 것처럼 이용하면 된다.
+
     }
