134f92f178efce88331f2b5fa92df2a08b73d7a6
Woochang1132 <show4031132@gmail.com>
2023-11-20 16:38:08 +0900

    typeScript => stack 챌린지, repeat

--- 4-oop-project/1-1-stack.ts
+++ 4-oop-project/1-1-stack.ts
@@ -2,56 +2,52 @@
 // 구현된 push, pop 사용하지 않고
 // 문자열로 구현
 
-
 interface Stack {
-    readonly size : number;
+    readonly _size : number;
     push(value : string): void;
     pop(): string;
 }
 
 type StackNode = {
-    readonly value : string;
-    readonly next?: StackNode;
+    value : string,
+    next ?: StackNode
 }
 
 class StackImp implements Stack{
-    private _size: number = 0;
-    private head?: StackNode;
-
-    constructor(private capacity : number){
-
-    }
+    _size: number = 0;
+    private head ?: StackNode
+    
     get size(){
         return this._size;
     }
-    
+
     push(value: string): void {
-        if(this.size === this.capacity){
-            throw new Error('Stack is full')
+        const node : StackNode = {
+            value : value,
+            next : this.head
         }
-        // Node로 감싸여져 있고 value가 전달 받은 value로 대체된다.
-        const node : StackNode = {value : value, next : this.head}
         this.head = node;
-        this._size++;
+        this._size++; 
     }
+
     pop(): string {
         if(this.head == null){
-            throw new Error('Stack is Empty!')
+            throw new Error('head state is null');
         }
         const node = this.head;
-        this.head = node.next;
-        this._size--;
+        this.head = node.next; // node.next도 결국엔 StackNode를 담고 있다.
+
+        this._size--; 
         return node.value;
     }
-  
 }
 
-const stack = new StackImp(10);
-stack.push('Ellie1');
-stack.push('Bob 2');
-stack.push('Steve 3');
+const stackResult = new StackImp()
 
-while(stack.size !==0){
-    console.log(stack.pop())
-}
+stackResult.push('최우창');
+stackResult.push('최진영');
+stackResult.push('최무식');
 
+stackResult.pop();
+
+console.log("stackResult >>", stackResult)
