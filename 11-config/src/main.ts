3809c338dc7b1a6c9542dbfd49737f973f14d139
Woochang1132 <show4031132@gmail.com>
2023-11-24 14:35:20 +0900

    typeScript =>  sourceMap을 활용한 browser debugging

--- 11-config/src/main.ts
+++ 11-config/src/main.ts
@@ -1,1 +1,14 @@
-console.log("haha")
+
+{
+class Car{
+    engine = 0;
+    move() {
+        const engine = this.engine + 1;
+        console.log('engine 🚓')
+        console.log(engine);
+    }
+}
+
+const car = new Car();
+car.move();
+}
